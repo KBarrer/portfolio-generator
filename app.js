039ac3e75ea21a62dbe9be1080bb8529eeafeed4
KSB <kyle.barrer@gmail.com>
2021-04-17 00:34:05 -0600

    added generate html

--- app.js
+++ app.js
@@ -1,18 +1,32 @@
-const profileDataArgs = process.argv.slice(2, process.argv.legth);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
-console.log(profileDataArgs);
+const profileDataArgs = process.argv.slice(2);
 
-
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }
-
-    console.log('=============================');
-
-    profileDataArr.forEach((profileItem) => console.log(profileItem));
-};
+const [givenName, github] = profileDataArgs;
 
 
 
-printProfileData(profileDataArgs);
+fs.writeFile('index.html', generatePage(givenName, github), err => {
+    if (err) throw new error(err);
+
+    console.log('Portfolio complete! Check out index.html to see the output!')
+});
+
+
+// console.log(profileDataArgs);
+
+
+// const printProfileData = profileDataArr => {
+//     for (let i = 0; i < profileDataArr.length; i++) {
+//         console.log(profileDataArr[i]);
+//     }
+
+//     console.log('=============================');
+
+//     profileDataArr.forEach((profileItem) => console.log(profileItem));
+// };
+
+
+
+// printProfileData(profileDataArgs);
