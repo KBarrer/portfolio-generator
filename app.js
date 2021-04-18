899967c8a6b03b0dd41bfd4d084cbd14c80250dc
KSB <kyle.barrer@gmail.com>
2021-04-17 23:16:28 -0600

    add inquirer

--- app.js
+++ app.js
@@ -1,32 +1,22 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-const profileDataArgs = process.argv.slice(2);
+// const pageHTML = generatePage(name, github);
 
-const [givenName, github] = profileDataArgs;
+// fs.writeFile('index.html', pageHTML, err => {
+//     if (err) throw err;
 
+//     console.log('Portfolio complete! Check out index.html to see the output!')
+// });
 
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'       
+        }
+    ])
 
-fs.writeFile('index.html', generatePage(givenName, github), err => {
-    if (err) throw new error(err);
-
-    console.log('Portfolio complete! Check out index.html to see the output!')
-});
-
-
-// console.log(profileDataArgs);
-
-
-// const printProfileData = profileDataArr => {
-//     for (let i = 0; i < profileDataArr.length; i++) {
-//         console.log(profileDataArr[i]);
-//     }
-
-//     console.log('=============================');
-
-//     profileDataArr.forEach((profileItem) => console.log(profileItem));
-// };
-
-
-
-// printProfileData(profileDataArgs);
+.then(answers => console.log(answers));
